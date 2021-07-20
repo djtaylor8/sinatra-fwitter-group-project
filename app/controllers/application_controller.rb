57fce930c20a7dd62be796ab0da6409226d7946b
DJ Taylor <dtaylor88@gmail.com>
2021-07-20 15:40:46 -0700

    commit tree 34e9c961206318662e6dbf9018ce7b5547de6e59

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,77 @@
 require './config/environment'
+require 'rack-flash'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions 
+    set :session_secret, "1cc568437824f34abff9dfdc958a11065626c4f5b59dfd1bfac320422afd24385705943e2e72b649a405501b680a96720133bbb9ba263140190b58c8c555164a"
+    use Rack::Flash
+  end
+
+  get '/' do 
+    erb :index
+  end
+  
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'/create_user'
+    end
+  end
+
+  post '/signup' do 
+    if params[:username] == '' || params[:email] == '' || params[:password] == ''
+      flash[:message] = "Error - Please Try Again!"
+      redirect '/users/signup'
+    else
+      @user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+      session[:user_id] = @user.id
+      flash[:message] = 'Success! User Created.'
+      redirect "/tweets"
+    end  
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else 
+    erb :'/login'
+    end
+  end
+
+  post '/login' do 
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect '/tweets'
+    else
+      flash[:message] = 'Uhoh, login failed. Please try again!'
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do 
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end 
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,83 @@
+require 'pry'
 class TweetsController < ApplicationController
+
+    get '/tweets' do
+        @tweets = Tweet.all 
+        if !logged_in?
+            redirect '/login'
+        else 
+            erb :'tweets/tweets'
+        end
+    end
+
+    get '/tweets/new' do
+        if !logged_in?
+            redirect '/login'
+        else   
+        erb :'/tweets/new'
+        end 
+    end
+
+    post '/tweets' do 
+      if params[:content] == ''
+        redirect '/tweets/new'
+      else
+       @tweet = Tweet.create(:content => params[:content])
+       @tweet.user_id = current_user.id
+       @tweet.save
+       redirect "/tweets/#{@tweet.id}" 
+      end  
+    end
+
+    get '/tweets/:id/edit' do
+        if !logged_in?
+          redirect '/login'
+        end
+        @tweet = Tweet.find(params[:id])
+        if @tweet.user_id != current_user.id
+            flash[:message] = "Sorry, you cannot edit tweets that are not yours!"
+            redirect '/tweets'
+        else
+          erb :'/tweets/edit_tweet'
+       end 
+    end
+
+    get '/tweets/:id' do
+      if !logged_in?
+        redirect '/login'
+      else
+        @tweet = Tweet.find(params[:id])
+        @user = current_user
+        erb :'/tweets/show_tweet'
+     end 
+    end
+
+    patch '/tweets/:id' do 
+      @tweet = Tweet.find(params[:id])
+        if params[:tweet][:content] == ''
+            redirect "/tweets/#{@tweet.id}/edit"
+        else 
+            @tweet.update(params[:tweet]) 
+            @tweet.user_id = current_user.id
+            @tweet.save 
+            redirect "/tweets/#{@tweet.id}"
+        end
+    end
+
+    delete '/tweets/:id/delete' do
+        if !logged_in? 
+            redirect '/login'
+        end 
+        @tweet = Tweet.find(params[:id])
+        if @tweet.id == current_user.id
+            @tweet.destroy
+            redirect '/tweets'
+        else
+            flash[:message] = "Sorry, you can't delete tweets that aren't yours!"
+            redirect '/tweets'
+        end
+        binding.pry 
+    end
 
 
 end
